3718d7172d022002c2b40c213a62c2a881baaa17
Deng Haolin <1020958553@qq.com>
2018-10-14 10:28:21 +0800

    add weather api
    request the weather info from api

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,20 @@
-Page({})
+Page({
+  data: {
+    temp: "20",
+    weather: "晴",
+    weather_bg: "",
+  },
+
+  onLoad() {
+    console.log('hello world!')
+
+    wx.request({
+      url: 'https://wthrcdn.etouch.cn/weather_mini',
+      data: {city: '深圳'},
+      method: 'GET',
+      success: res => {
+        console.log(res)
+      }
+    })
+  }
+})
