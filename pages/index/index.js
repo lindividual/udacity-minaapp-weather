068525b4e9c79c05ef88ad41140826d75b780c00
Deng Haolin <1020958553@qq.com>
2018-10-14 12:26:49 +0800

    changing the bg and bar color
    根据天气类型动态更换背景图片和顶部栏颜色

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,21 @@
+const weatherMap = {
+  '多云': { name: 'cloudy', color: '#deeef6'},
+  '阴': {
+    name: 'overcast', color: '#c6ced2'},
+  '小雨': {
+    name: 'lightrain', color: '#bdd5e1'},
+  '阵雨': {
+    name: 'lightrain', color: '#bdd5e1'},
+  '雷阵雨': {
+    name: 'heavyrain', color: '#c5ccd0'},
+  '晴': {
+    name: 'sunny', color: '#cbeefd'}
+}
+
 Page({
   data: {
-    temp: "20",
-    weather: "晴",
+    temp: "",
+    weather: "",
     weather_bg: "",
   },
 
@@ -10,10 +24,24 @@ Page({
 
     wx.request({
       url: 'https://wthrcdn.etouch.cn/weather_mini',
-      data: {city: '深圳'},
+      // 请求数据写在data中
+      data: {city: '南京'},
       method: 'GET',
       success: res => {
         console.log(res)
+        let result = res.data.data
+        let temp = result.wendu
+        let weather = result.forecast[0].type
+        console.log(temp, weather)
+        this.setData({
+          temp: temp,
+          weather: weather,
+          weather_bg: '/images/'+ weatherMap[weather].name +'-bg.png'
+        })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherMap[weather].color,
+        })
       }
     })
   }
